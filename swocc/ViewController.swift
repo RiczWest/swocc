76352d36625546644a3d48dceba4ce540077dea3
Richard West <rik.west@yahoo.co.uk>
2015-04-19 12:48:12 +0100

    Initial Commit

---
+++ swocc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swocc
+//
+//  Created by Richard West on 19/04/2015.
+//  Copyright (c) 2015 Richard West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
